52f567519fcf7ed96adcaef55eeb6101274ad1f9
Dellucifer <pg99285@gmail.com>
2022-10-20 00:43:23 +0530

    Added News Project in ReactJS

---
+++ News App/src/App.js
@@ -0,0 +1,42 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+import News from "./components/News";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LoadingBar from 'react-top-loading-bar'
+
+const App = ()=>{
+  const pageSize = 9;
+  const apiKey = process.env.REACT_APP_NEWS_API
+  // const apiKey = "5687cea9ba474e18aafc836e79ed64bc"
+
+  const [progress, setProgress] = useState(0)
+
+    return (
+      <div>
+        <Router>
+          <Navbar />
+
+          <LoadingBar
+        color='#f11946'
+        progress={progress}
+        height={3}
+        // onLoaderFinished={() => setProgress(0)}
+      />
+      
+          <Routes>
+            <Route exact path='/' element={<News setProgress={setProgress} apiKey={apiKey} key="general" pageSize={pageSize} country="in" category="general" />} />
+            <Route exact path='/business' element={<News setProgress={setProgress} apiKey={apiKey} key="business" pageSize={pageSize} country="in" category="business" />} />
+            <Route exact path='/entertainment' element={<News setProgress={setProgress} apiKey={apiKey} key="entertainment" pageSize={pageSize} country="in" category="entertainment" />} />
+            <Route exact path='/health' element={<News setProgress={setProgress} apiKey={apiKey} key="health" pageSize={pageSize} country="in" category="health" />} />
+            <Route exact path='/science' element={<News setProgress={setProgress} apiKey={apiKey} key="science" pageSize={pageSize} country="in" category="science" />} />
+            <Route exact path='/sports' element={<News setProgress={setProgress} apiKey={apiKey} key="sports" pageSize={pageSize} country="in" category="sports" />} />
+            <Route exact path='/technology' element={<News setProgress={setProgress} apiKey={apiKey} key="technology" pageSize={pageSize} country="in" category="technology" />} />
+          </Routes>
+        
+        </Router>
+      </div>
+    );
+}
+
+export default App;
